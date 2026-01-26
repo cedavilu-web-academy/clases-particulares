8aa3cc307ba8f23a081b92a1e523d3ede4432c4b
cedavilu-web-academy <angel.daniel.fuentes.segura@gmail.com>
2026-01-26 12:43:55 -0300

    Actualizado a la clase 5

---
+++ clase5/js/script.js
@@ -0,0 +1,34 @@
+// Trabajando con las variables
+let nombre = 'Daniel';
+let apellido = 'Fuentes';
+let edad = 58;
+let esProfesor = true;  // false - true
+
+// Debugger - Verificar si existe algún error o no
+console.log(typeof(nombre));
+console.log(typeof(apellido));
+console.log(typeof(edad));
+console.log(typeof(esProfesor));
+
+// Operadores: 
+// Aritméticos: + - * /
+// Relacionales - Comparación: > < >= <= != == ===
+// Lógicos: AND (y) && - OR (o) || - NOT (no) !
+
+
+// Desarrollar un programa en JavaScript, que permita pedir al usuario dos valores
+// Con los valores suministrados: deben efectuar la sumatoria de los datos 
+// Mostrar el resultado por la pantalla
+
+// Opcional: Pedir al usuario el tipo de operación: sumar + restar + multiplicar + dividir
+/*
+    Indique valor 1:
+    Indique valor 2:
+    Indique operación ('sumar - restar - multiplicar - dividir')
+    Debe hacer el cálculo
+    Mostrar el resultado
+*/
+
+// Repasar: Variables - Operadores - Condiciones - Arreglos - Arrays - Vectores
+
+
