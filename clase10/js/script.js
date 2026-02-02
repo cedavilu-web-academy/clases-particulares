5bf6c23654c5a9961903405138ce4a0c34d889a2
cedavilu-web-academy <angel.daniel.fuentes.segura@gmail.com>
2026-02-02 12:37:36 -0300

    Actualizada la clase 10

--- clase10/js/script.js
+++ clase10/js/script.js
@@ -15,6 +15,40 @@ console.log(botones);
 let botonOscuro = document.getElementById('botonOscuro');
 let botonClaro = document.getElementById('botonClaro');
 
+let botonMoto = document.getElementById('botonMoto');
+let botonCarro = document.getElementById('botonCarro');
+
+
 // Existe otra forma para capturar los elementos del DOM
 // Me permite capturar por Etiqueta (h1 - h2 - p - ul - body) - por ID ('#id' ) y por clase 
 // ('.clase')
+
+// Invocando los eventos desde JavaScript
+// botonOscuro.addEventListener('click', modoOscuro);
+// botonClaro.addEventListener('click', modoClaro);
+
+
+// Invocando los eventos desde JavaScript - De otra forma
+//                                  Callback()
+botonMoto.addEventListener('mouseout', function(){
+    body.classList.remove('fondoCarro');
+    body.classList.add('fondoMoto');
+})
+
+botonCarro.addEventListener('mouseout', function(){
+    body.classList.remove('fondoMoto')
+    body.classList.add('fondoCarro')
+})
+
+
+
+// Creando funciones
+function modoOscuro() {
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';
+}
+
+function modoClaro() {
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+}
